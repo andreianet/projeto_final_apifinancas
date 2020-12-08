8dd96a035ae974a70bce25ce0af5a834c38a5ff3
andreianet <andreia.netsys@hotmail.com>
2020-12-08 16:48:28 -0300

    corrigindo schema

--- src/models/categorySchema.js
+++ src/models/categorySchema.js
@@ -8,7 +8,7 @@ const categorySchema = new Schema({
         require: true
     },
     name:{
-        type:['Alimentação','Farmacia','Colegio','Vestuario'],
+        type:['Alimentacao','Farmacia','Colegio','Vestuario'],
         required: true
     },
 })
