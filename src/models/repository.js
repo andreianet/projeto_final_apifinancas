82ed392be098d50c2d4ac202219405a10c502d41
andreianet <andreia.netsys@hotmail.com>
2020-12-09 19:32:05 -0300

    corrigido dotenv

--- src/models/repository.js
+++ src/models/repository.js
@@ -1,6 +1,8 @@
-require('dotenv-safe').config();
-//const app = require('../../src/app')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+dotenv.config();
+//const app = require('../../src/app')
+
 
 //String Connection
 const connect = () => {
