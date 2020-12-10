85bd9cb593fd47a29e869626678c9cc851a21df1
andreianet <andreia.netsys@hotmail.com>
2020-12-09 21:51:39 -0300

    corrigido serverjs e removido app+repo

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
-const app = require('./src/app')
+
+const mongoose = require('mongoose')
 const dotenv = require('dotenv');
+const express = require("express");
+const cors = require('cors')
+const app = express();
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000
+
+//rotas
+const index = require('./src/routes/index');
+const expense = require('./src/routes/expenseRoute')
+const category = require('./src/routes/categoryRoute')
+const finance = require('./src/routes/financeRoute')
+
+//String Connection
+
+    mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true }) //parsea a conexão 
+    const connection = mongoose.connection
+    //caso der um erro 
+    connection.on('error', ()=> console.error("Error Connection"))
+    connection.once('open', ()=> console.log("Conncetion MongoDB Success..."))
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", index);
+app.use("/expense", expense);
+app.use("/category", category);
+app.use("/finance", finance);
 
 app.listen(PORT, function () {
     console.log(`Server OK!Na porta:${PORT}`);
