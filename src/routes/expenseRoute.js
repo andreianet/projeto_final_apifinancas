c1fb92ed6757248dc65d7421c5b7dcb12fbc1cdc
andreianet <andreia.netsys@hotmail.com>
2020-12-11 17:53:40 -0300

    atualizado

--- src/routes/expenseRoute.js
+++ src/routes/expenseRoute.js
@@ -6,7 +6,7 @@ const controller = require('../controllers/expenseControllers')
  * @route GET /
  * @desc Retornar todas despesas(expense)
  * @access Public
- * @endpoint htt://localhost:porta/expense
+ * @endpoint htt://localhost:porta/expense/allexpense
  */
 router.get('/allexpense', controller.getAllExpense)
 
